1c6e0f2947e4710646a724fe762e67c5004ed86d
Muhammad Taimur Adil <taimur.adil93@gmail.com>
2022-11-07 19:15:35 +0500

    integrate mlflow

--- businessml/businessml_api/api/app_routes.py
+++ businessml/businessml_api/api/app_routes.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, current_app, jsonify
+from random import randint, random
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
 
 http = Blueprint(name="businessml", import_name=__name__, url_prefix="/api")
 
@@ -6,3 +9,13 @@ http = Blueprint(name="businessml", import_name=__name__, url_prefix="/api")
 def application_status():
     return jsonify({'status':'Server is up and running'})
 
+@http.route('/sample_run')
+def sample_run():
+    
+    mlflow.set_tracking_uri('http://mlflow:5000')
+    log_param("param1", randint(0, 100))
+    log_metric("foo", random())
+    log_metric("foo", random() +1)
+    log_metric("foo", random() +2)
+
+    return jsonify({'status':'sample eun execution completed,  ${MLFLOW_TRACKING_URI}'})
